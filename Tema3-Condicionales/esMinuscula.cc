d6a276515d3b4f44293256563e735d1b16e0b257
Francisco Martínez del Río <fmartin@ujaen.es>
2017-06-26 19:53:09 +0200

    Indentación

--- Tema3-Condicionales/esMinuscula.cc
+++ Tema3-Condicionales/esMinuscula.cc
@@ -3,8 +3,8 @@
 using namespace std;
 
 /* Programa: esMinuscula
- * Entradas: Un car·cter
- * Salidas: Indica si el car·cter es una vocal min˙scula
+ * Entradas: Un car√°cter
+ * Salidas: Indica si el car√°cter es una vocal min√∫scula
  */
 
 int main ()
@@ -13,22 +13,21 @@ int main ()
 	cout << "Introduce una letra: ";
 	cin >> letra;
 	if (letra == 'a' || letra == 'e' || letra == 'i' || letra ==  'o' || letra == 'u')
-		cout << "Has introducido una vocal min˙scula\n";
+		cout << "Has introducido una vocal min√∫scula\n";
 	else
-		cout << "No has introducido una vocal min˙scula\n";
+		cout << "No has introducido una vocal min√∫scula\n";
 
     // Otra forma de resolverlo
-    switch (letra) {
-        case 'a':
-        case 'e':
-        case 'i':
-        case 'o':
-        case 'u':
-            cout << "Has introducido una vocal min˙scula\n";
-            break;
-        default:
-            cout << "No has introducido una vocal min˙scula\n";
-    }
+	switch (letra) {
+        	case 'a':
+        	case 'e':
+        	case 'i':
+        	case 'o':
+        	case 'u':
+            		cout << "Has introducido una vocal min√∫scula\n";
+            		break;
+        	default:
+            		cout << "No has introducido una vocal min√∫scula\n";
+    	}
 	return 0;
 }
-
