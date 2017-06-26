0aa88d7156bcba56dff23f450732cf4db2dca784
Francisco Martínez del Río <fmartin@ujaen.es>
2017-06-26 19:49:45 +0200

    Se cambian tabulaciones

--- Tema3-Condicionales/max_3_numeros.cc
+++ Tema3-Condicionales/max_3_numeros.cc
@@ -2,15 +2,15 @@
 
 using namespace std;
 
-/* Programa: MáximoTresNúmeros
- * Entradas: Tres números reales
- * Salidas: El máximo de los tres números
+/* Programa: MÃ¡ximoTresNÃºmeros
+ * Entradas: Tres nÃºmeros reales
+ * Salidas: El mÃ¡ximo de los tres nÃºmeros
  */
 
 int main ()
 {
 	double a, b, c;
-	cout << "Introduzca tres números separados por espacios: ";
+	cout << "Introduzca tres nÃºmeros separados por espacios: ";
 	cin >> a >> b >> c;
 	// Forma 1
 	if (a > b) {
@@ -29,11 +29,11 @@ int main ()
 	// Forma 2
 	if (a >= b && a >= c) {
         cout << "El mayor calculado de la segunda forma es " << a << endl;
-    } else if (b > c) {
- 			cout << "El mayor calculado de la segunda forma es " << b << endl;
-    } else {
+    	} else if (b > c) {
+ 		cout << "El mayor calculado de la segunda forma es " << b << endl;
+    	} else {
 		cout << "El mayor calculado de la segunda forma es " << c << endl;
-    }
+    	}
 	// Forma 3
 	double max = a > b ? (a > c ? a : c) : (b > c ? b : c);
 	cout << "El mayor calculado de la tercera forma: " << max << endl;
