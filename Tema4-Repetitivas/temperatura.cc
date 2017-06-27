0c085e9d742a8af904a1c8f944c38f3ed3654547
Francisco Martínez del Río <fmartin@ujaen.es>
2017-06-27 18:15:06 +0200

    se añade espacio en blanco

--- Tema4-Repetitivas/temperatura.cc
+++ Tema4-Repetitivas/temperatura.cc
@@ -2,18 +2,18 @@
 #include <limits>
 using namespace std;
 
-/* Programa: EstadÌsticaTemperaturas
- * Entradas: Una serie de  6 tomas de temperatura realizadas en un dÌa
- * Salidas: El m·ximo, mÌnimo y media de las tomas
+/* Programa: Estad√≠sticaTemperaturas
+ * Entradas: Una serie de  6 tomas de temperatura realizadas en un d√≠a
+ * Salidas: El m√°ximo, m√≠nimo y media de las tomas
  */
 
 int main ()
 {
-	const int NTOMAS = 6;                         // n˙mero de tomas.
-	double max = -numeric_limits<double>::max (); // temp. m·xima. Se inicia al menor double posible
-	double min = numeric_limits<double>::max ();  // temp. mÌnima. Se inicia al mayor double posible
+	const int NTOMAS = 6;                         // n√∫mero de tomas.
+	double max = -numeric_limits<double>::max (); // temp. m√°xima. Se inicia al menor double posible
+	double min = numeric_limits<double>::max ();  // temp. m√≠nima. Se inicia al mayor double posible
 	double media = 0;
-	for (int i = 1; i<= NTOMAS; i++) {
+	for (int i = 1; i <= NTOMAS; i++) {
 		double temperatura;
 		cout << "Introduzca la temperatura " << i << ": ";
 		cin >> temperatura;
@@ -23,8 +23,8 @@ int main ()
 			min = temperatura;
 		media += temperatura;
 	}
-	cout << "Temperatura m·xima: " << max << endl;
-	cout << "Temperatura mÌnima: " << min << endl;
+	cout << "Temperatura m√°xima: " << max << endl;
+	cout << "Temperatura m√≠nima: " << min << endl;
 	cout << "Temperatura media: " << media / NTOMAS << endl;
 	return 0;
 }
