d0449c497e08ae7f039ad87912d25aa3459344b7
Francisco Martínez del Río <fmartin@ujaen.es>
2017-07-12 13:44:05 +0200

    Alternativa a operador +=

--- Tema4-Repetitivas/suma_cuadrados.cc
+++ Tema4-Repetitivas/suma_cuadrados.cc
@@ -9,7 +9,7 @@ int main ()
 {
 	int suma = 0;
 	for (int i = 1; i <= 10; ++i)
-		suma += i*i;
+		suma += i*i; // También vale suma = suma + i*i;
 	std::cout << "La suma de los cuadrados de los 10 primeros enteros positivos es " << suma << '\n';
 	return 0;
 }
