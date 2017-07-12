63dcdb83563d17c415b3cf631f83064bb1330061
Francisco Martínez del Río <fmartin@ujaen.es>
2017-07-12 13:47:20 +0200

    Corregido error tipográfico

--- Tema4-Repetitivas/hastaEnRango.cc
+++ Tema4-Repetitivas/hastaEnRango.cc
@@ -2,9 +2,9 @@
 using namespace std;
 
 /* Programa: hastaEnRango
- * Entradas: Una serie de números hasta que se introduzca un valor el rango [20,30]
+ * Entradas: Una serie de nÃºmeros hasta que se introduzca un valor en el rango [20,30]
  *           o el valor 0.
- * Salidas: El último valor introducido
+ * Salidas: El Ãºltimo valor introducido
  */
 
 int main ( )
@@ -14,7 +14,7 @@ int main ( )
         cout << "Introduce un valor en el rango [20,30] o el 0: ";
         cin >> n;
     } while (n > 30 || (n < 20 && n != 0));
-    // Una condición alternativa es (n != 0 && !(n>=20 && n <= 30))
+    // Una condiciÃ³n alternativa es (n != 0 && !(n>=20 && n <= 30))
     cout << "El valor introducido correcto es " << n << endl;
     return 0;
 }
