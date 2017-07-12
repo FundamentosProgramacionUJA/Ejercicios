1954f1d8ec902de9e422b25f68481fd0c1d82e95
Francisco Martínez del Río <fmartin@ujaen.es>
2017-07-12 19:13:31 +0200

    Se incluye espacio en blanco para facilitar la lectura

--- Tema4-Repetitivas/aproximacionPI.cc
+++ Tema4-Repetitivas/aproximacionPI.cc
@@ -2,19 +2,19 @@
 #include <cmath>
 using namespace std;
 
-/* Programa: AproximaciónPI
- * Entradas: Número de términos de la sumatoria especificada en el enunciado
- * Salidas: Una aproximación al número PI
+/* Programa: AproximaciÃ³nPI
+ * Entradas: NÃºmero de tÃ©rminos de la sumatoria especificada en el enunciado
+ * Salidas: Una aproximaciÃ³n al nÃºmero PI
  */
 
 int main () {
     double suma = 0;
     int nterm;
-    cout << "Introduzca el número de términos: ";
+    cout << "Introduzca el nÃºmero de tÃ©rminos: ";
     cin >> nterm;
-    for (int i = 1; i<= nterm; ++i) {
-        suma = suma + 6/double(i*i); // también vale suma += 6/double(i*i)
+    for (int i = 1; i <= nterm; ++i) {
+        suma = suma + 6/double(i*i); // tambiÃ©n vale suma += 6/double(i*i)
     }
-    cout << "La aproximación es " << sqrt(suma) << '\n';
+    cout << "La aproximaciÃ³n es " << sqrt(suma) << '\n';
     return 0;
 }
