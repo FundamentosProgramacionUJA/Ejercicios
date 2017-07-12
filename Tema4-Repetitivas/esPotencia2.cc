b0d816218524b279945a10c09bdd38460ab6cad4
franciscomartinezdelrio <fmartin@ujaen.es>
2017-07-12 19:43:44 +0200

    Programa para calcular si un entero es potencia de 2

---
+++ Tema4-Repetitivas/esPotencia2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+
+/* Programa: EsPotencia2
+ * Entradas: Un número entero
+ * Salidas: Un mensaje indicando si el número es una potencia de dos
+ */
+
+int main ()
+{
+    int numero;
+    cout << "Introduzca un entero: ";
+    cin >> numero;
+    int x = 1;
+    while (x < numero)
+        x = x * 2;      // también vale x *= 2;
+    if (x == numero)
+        cout << numero << " es una potencia de dos\n";
+    else
+        cout << numero << " no es una potencia de dos\n";
+	return 0;
+}
+
