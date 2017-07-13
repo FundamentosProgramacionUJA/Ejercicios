f877ca2c44557d09d201563bf1aa8363a6812c97
Francisco Martínez del Río <fmartin@ujaen.es>
2017-07-13 18:38:10 +0200

    Indentación

--- Tema5-Tipos de datos estructurados/suma_matrices.cc
+++ Tema5-Tipos de datos estructurados/suma_matrices.cc
@@ -1,11 +1,10 @@
 /* ---------------------------------------------------------------------------
  * Programa: suma_matrices
- * Entradas: Dos matrices 3x3 de números
+ * Entradas: Dos matrices 3x3 de números (realmente se definen en el programa)
  * Salidas:  La suma de las dos matrices
  * --------------------------------------------------------------------------- */
 
 #include <iostream>
-
 using namespace std;
 
 int main ()
@@ -21,7 +20,7 @@ int main ()
 		for (int c = 0; c < 3; c++)
 			r[f][c] = m1[f][c] + m2[f][c];
 
-    // se muestra la suma en la salida estándar
+        // se muestra la suma en la salida estándar
 	for (int f = 0; f < 3; f++) {
 		for (int c = 0; c < 3; c++)
 			cout << r[f][c] << ' ';
