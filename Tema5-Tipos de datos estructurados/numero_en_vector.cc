c621e47725630f286693c8e5f9bd73c3115db835
franciscomartinezdelrio <fmartin@ujaen.es>
2017-07-13 17:49:50 +0200

    Se incluye ejercicio busca un dato en un vector

---
+++ Tema5-Tipos de datos estructurados/numero_en_vector.cc
@@ -0,0 +1,30 @@
+/* ---------------------------------------------------------------------------
+ * Programa: numero_en_vector
+ * Entradas: Un valor entero
+ * Salidas:  Un mensaje indicando si el entero coincide con los elementos de
+ *           un vector prefijado
+ * --------------------------------------------------------------------------- */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+{
+    const int NDATOS = 6;
+	int datos[NDATOS]= {1, 3, 5, 7, 11, 20};
+	int n;
+    cout << "Introduzca un valor entero: ";
+    cin >> n;
+    bool esta;
+    for (int i = 0; i < NDATOS; ++i) {
+        if (datos[i] == n)
+            esta = true;
+    }
+    if (esta)
+        cout << n << " se encuentra en el vector\n";
+    else
+        cout << n << " no se encuentra en el vector\n";
+	return 0;
+}
