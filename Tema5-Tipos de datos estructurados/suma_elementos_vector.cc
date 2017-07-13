79ab6f71fa1b02427019c08d93ce799ee21c82e1
Francisco Martínez del Río <fmartin@ujaen.es>
2017-07-13 17:20:26 +0200

    Indentación

--- Tema5-Tipos de datos estructurados/suma_elementos_vector.cc
+++ Tema5-Tipos de datos estructurados/suma_elementos_vector.cc
@@ -1,11 +1,10 @@
 /* ---------------------------------------------------------------------------
  * Programa: suma_elementos_vector
- * Entradas: Un vector de números (aunque realmente se define en el programa)
+ * Entradas: Un vector de nÃºmeros (aunque realmente se define en el programa)
  * Salidas:  La suma de los elementos del vector
  * --------------------------------------------------------------------------- */
 
 #include <iostream>
-
 using namespace std;
 
 int main ()
@@ -13,8 +12,8 @@ int main ()
     const int TAM = 5;
     double v[TAM] = { 1, 1.5, 2, 0, 4};
     double suma = 0;
-	for (int i = 0; i < TAM; i++)
-        suma = suma + v[i]; // también vale: suma += v[i];
-	cout << "La suma de los elementos del vector es " << suma << endl;
-	return 0;
+    for (int i = 0; i < TAM; i++)
+        suma = suma + v[i]; // tambiÃ©n vale: suma += v[i];
+    cout << "La suma de los elementos del vector es " << suma << endl;
+    return 0;
 }
