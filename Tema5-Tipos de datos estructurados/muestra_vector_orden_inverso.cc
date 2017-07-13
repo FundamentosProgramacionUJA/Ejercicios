5bf7ee4038682a7ff409308a7cd7e08cc0bdfa71
Francisco Martínez del Río <fmartin@ujaen.es>
2017-07-13 17:17:50 +0200

    Indentación

--- Tema5-Tipos de datos estructurados/muestra_vector_orden_inverso.cc
+++ Tema5-Tipos de datos estructurados/muestra_vector_orden_inverso.cc
@@ -1,20 +1,19 @@
 /* ---------------------------------------------------------------------------
  * Programa: muestra_vector_orden_inverso
- * Entradas: Un vector de números (aunque realmente se define en el programa)
+ * Entradas: Un vector de nÃºmeros (aunque realmente se define en el programa)
  * Salidas:  Los elementos del vector en orden inverso
  * --------------------------------------------------------------------------- */
 
 #include <iostream>
-
 using namespace std;
 
 int main ()
 {
     const int TAM = 5;
     double v[TAM] = { 1, 1.5, 2, 0, 4};
-	for (int i = TAM-1; i >= 0; i--) {
+    for (int i = TAM-1; i >= 0; i--) {
         cout << v[i] << " ";
-	}
-	cout << endl;
-	return 0;
+    }
+    cout << endl;
+    return 0;
 }
