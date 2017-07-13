00ec199f8245b3b0d9832d8ad3b818e6fe1ca31a
Francisco Martínez del Río <fmartin@ujaen.es>
2017-07-13 17:23:59 +0200

    Indentación

--- Tema5-Tipos de datos estructurados/maximovector.cc
+++ Tema5-Tipos de datos estructurados/maximovector.cc
@@ -5,30 +5,29 @@
  * --------------------------------------------------------------------------- */
 
 #include <iostream>
-
 using namespace std;
 
 int main ()
 {
     const int TAMMAX = 1000;
-	int v[TAMMAX];
-	int nel; // número de elemntos del vector
-	do {
+    int v[TAMMAX];
+    int nel; // número de elemntos del vector
+    do {
         cout << "Introduzca el número de elementos del vector: ";
         cin >> nel;
-	} while (nel < 1 || nel > TAMMAX);
+    } while (nel < 1 || nel > TAMMAX);
 
     // lectura de los elementos del vector
-	for (int i = 0; i < nel; ++i) {
-		cout << "Introduzca el elemento " << i+1 << ": ";
-		cin >> v[i];
-	}
+    for (int i = 0; i < nel; ++i) {
+	cout << "Introduzca el elemento " << i+1 << ": ";
+	cin >> v[i];
+    }
     // cálculo del máximo
-	double maximo = v[0];
-	for (int i = 1; i < nel; ++i) {
-		if (v[i] > maximo)
-			maximo = v[i];
+    double maximo = v[0];
+    for (int i = 1; i < nel; ++i) {
+	if (v[i] > maximo)
+	    maximo = v[i];
 	}
-	cout << "El máximo es " << maximo << endl;
-	return 0;
+    cout << "El máximo es " << maximo << endl;
+    return 0;
 }
