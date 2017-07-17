4418ba2378815ebca31a710f36987e07ad354405
Francisco Martínez del Río <fmartin@ujaen.es>
2017-07-17 10:55:40 +0200

    Indentación

--- Tema5-Tipos de datos estructurados/imc_personas.cc
+++ Tema5-Tipos de datos estructurados/imc_personas.cc
@@ -1,7 +1,8 @@
 /* ---------------------------------------------------------------------------
  * Programa: imc_personas
  * Entradas: Nombre, peso y altura de una serie de personas
- * Salidas:  Distintas estadísticas sobre los datos leídos como el IMC de cada persona
+ * Salidas:  Distintas estadísticas sobre los datos leídos como el IMC de cada 
+ *           persona
  * --------------------------------------------------------------------------- */
 
 #include <iostream>
@@ -21,18 +22,18 @@ int main ()
 	Persona p[TAMMAX];
 	int np;  // número de personas con las que se trabajan
 	do {
-        cout << "Indica el número de personas que vas a introducir: ";
-        cin >> np;
+        	cout << "Indica el número de personas que vas a introducir: ";
+        	cin >> np;
 	} while (np < 0 || np > TAMMAX);
 	// LECTURA DE LOS DATOS
 	for (int i = 0; i < np; ++i) {
-        cout << "Datos de la persona " << i+1 << ":\n";
-        cout << "\tNombre: ";
-        getline(cin >> ws, p[i].nombre);
-        cout << "\tAltura en metros: ";
-        cin >> p[i].altura;
-        cout << "\tPeso en kilogramos: ";
-        cin >> p[i].peso;
+        	cout << "Datos de la persona " << i+1 << ":\n";
+        	cout << "\tNombre: ";
+        	getline(cin >> ws, p[i].nombre);
+        	cout << "\tAltura en metros: ";
+        	cin >> p[i].altura;
+        	cout << "\tPeso en kilogramos: ";
+        	cin >> p[i].peso;
 	}
 	// SE MUESTRAN LOS DATOS DE LAS PERSONAS Y SE CALCULAN ESTADÍSTICAS
 	double imcMax = 0;        // mayor IMC
@@ -43,35 +44,34 @@ int main ()
 
 	cout << "\nListado de personas:\n";
 	for (int i = 0; i < np; ++i) {
-        double imc = p[i].peso/pow(p[i].altura,2);
-        cout << '\t' << p[i].nombre << " (" << p[i].altura << "m, " << p[i].peso
-             << "kg). IMC = " << imc << endl;
-        if (imc > imcMax) {
-            imcMax = imc;
-            indImcMax = i;
-        }
-        if (imc < imcMin) {
-            imcMin = imc;
-            indImcMin = i;
-        }
-        if (p[i].altura < p[indAlturaMinima].altura)
-            indAlturaMinima = i;
+        	double imc = p[i].peso/pow(p[i].altura,2);
+        	cout << '\t' << p[i].nombre << " (" << p[i].altura << "m, " << p[i].peso
+             	     << "kg). IMC = " << imc << endl;
+        	if (imc > imcMax) {
+            		imcMax = imc;
+            		indImcMax = i;
+        	}
+        	if (imc < imcMin) {
+            		imcMin = imc;
+            		indImcMin = i;
+        	}
+        	if (p[i].altura < p[indAlturaMinima].altura)
+            		indAlturaMinima = i;
 	}
 	cout << "\nEstadísticas de las personas:\n";
 	cout << "\tLa persona con mayor IMC es " << p[indImcMax].nombre << '\n';
 	if (indAlturaMinima == indImcMin)
-        cout << "\tLa persona más baja tiene el IMC menor\n";
-    else
-        cout << "\tLa persona más baja no tiene el IMC menor\n";
+        	cout << "\tLa persona más baja tiene el IMC menor\n";
+    	else
+        	cout << "\tLa persona más baja no tiene el IMC menor\n";
 
-    // SE CALCULA LAS PERSONAS QUE SUPERAN UNA ALTURA
-    cout << "\nIntroduce una altura: ";
-    double altura;
-    cin >> altura;
-    cout << "\nPersonas con más de " << altura << " metros:\n";
+    	// SE CALCULA LAS PERSONAS QUE SUPERAN UNA ALTURA
+    	cout << "\nIntroduce una altura: ";
+    	double altura;
+    	cin >> altura;
+    	cout << "\nPersonas con más de " << altura << " metros:\n";
 	for (int i = 0; i < np; ++i)
         if (p[i].altura > altura)
-            cout << '\t' << p[i].nombre << '\n';
-
+        	cout << '\t' << p[i].nombre << '\n';
 	return 0;
 }
