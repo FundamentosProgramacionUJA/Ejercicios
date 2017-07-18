85a87be5dab43e3ea5d3ca8dea8e3996cae9c8cf
Francisco Martínez del Río <fmartin@ujaen.es>
2017-07-18 10:44:39 +0200

    Indentación

--- Tema6-Funciones/compara_insensible_mayusculas.cc
+++ Tema6-Funciones/compara_insensible_mayusculas.cc
@@ -4,30 +4,31 @@
 
 using namespace std;
 
-/* Descripción: Calcula si dos strings son iguales sin tener en cuenta si las letras están en mayúsculas o en minúsculas
+/* Descripción: Calcula si dos strings son iguales sin tener en cuenta si las letras están 
+*               en mayúsculas o en minúsculas
  * Parámetros de entrada:
  *   - s1: el primer string
  *   - s2: el segundo string
  * Valor de retorno: verdadero si s1 == s2 sin tener en cuenta si las letras de los strings están en
  *                   mayúsculas o en minúsculas. Falso en otro caso
  */
-bool iguales (const string& s1, const string& s2)
+bool iguales(string s1, string s2)
 {
-	if (s1.length () != s2.length ())
+	if (s1.length () != s2.length())
 		return false;
-	for (int i = 0; i < s1.length (); i++)
+	for (int i = 0; i < s1.length(); i++)
 		if (toupper (s1[i]) != toupper (s2[i]))
 			return false;
 	return true;
 }
 
-int main ()
+int main()
 {
 	string s1, s2;
 	cout << "Introduzca una cadena: ";
-	getline (cin , s1);
+	getline(cin , s1);
 	cout << "Introduzca una segunda cadena: ";
-	getline (cin , s2);
-	cout << (iguales (s1, s2) ? "Iguales\n" : "Diferentes\n");
+	getline(cin , s2);
+	cout << (iguales(s1, s2) ? "Iguales\n" : "Diferentes\n");
 	return 0;
 }
