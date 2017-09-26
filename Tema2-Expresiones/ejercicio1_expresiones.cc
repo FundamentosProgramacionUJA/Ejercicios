27713f845f00c61a7e4151964a7029b6a9ae684d
Francisco Martínez del Río <fmartin@ujaen.es>
2017-09-26 09:56:23 +0200

    Indentación

--- Tema2-Expresiones/ejercicio1_expresiones.cc
+++ Tema2-Expresiones/ejercicio1_expresiones.cc
@@ -12,18 +12,18 @@ int main ()
 {
     double a = 2, b = 1, c = -1, d = 4, e = 4, f = 2;
     // Expresión a)
-	cout << a / b + 1 << endl;
+    cout << a / b + 1 << endl;
     // Expresión b)
-	cout << (a + b) / (c + d) << endl;
+    cout << (a + b) / (c + d) << endl;
     // Expresión c)
-	cout << (a + b/c) / (d + e/f) << endl;
+    cout << (a + b/c) / (d + e/f) << endl;
     // Expresión d)
-	cout << a + b / (c - d) << endl;
+    cout << a + b / (c - d) << endl;
     // Expresión e)
-	cout << (a + b) *  c / d << endl;
+    cout << (a + b) *  c / d << endl;
     // Expresión f)
-	cout << (-b+sqrt(b*b - 4*a*c))/(2*a) << endl;
-	cout << (-b-sqrt(b*b - 4*a*c))/(2*a) << endl;
+    cout << (-b+sqrt(b*b - 4*a*c))/(2*a) << endl;
+    cout << (-b-sqrt(b*b - 4*a*c))/(2*a) << endl;
 
-	return 0;
+    return 0;
 }
