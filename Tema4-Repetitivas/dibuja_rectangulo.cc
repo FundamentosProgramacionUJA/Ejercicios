7454444f68163ba9a2534c07720ae32b0365f692
franciscomartinezdelrio <fmartin@ujaen.es>
2018-09-13 12:47:35 +0200

    dibuja_rectangulo.cc added

---
+++ Tema4-Repetitivas/dibuja_rectangulo.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/* Programa: Dibuja rectángulo
+ * Entradas: Un entero positivo n
+ * Salidas: Dibuja un triángulo rectángulo de base y altura n
+ */
+int main() {
+    int n;
+    do {
+        std::cout << "Introduce un entero positivo: ";
+        std::cin >> n;
+    } while (n <= 0);
+    for (int i = 1; i <= n; i++) {
+        for (int x = 1; x <= i; x++)
+            std::cout << "* ";
+        std::cout << "\n";
+    }
+	return 0;
+}
