9de85a9bca0c32d1e24d6b09eca598313ac63705
Francisco Martínez del Río <fmartin@ujaen.es>
2018-09-27 11:22:54 +0200

    Añadir comentarios

--- Tema2-Expresiones/intercambio_variables.cc
+++ Tema2-Expresiones/intercambio_variables.cc
@@ -8,9 +8,11 @@
 int main ()
 {
 	double x, y;
+	// Se leen dos valores de la entrada estándar
 	std::cout << "Introduzca dos valores numéricos: ";
 	std::cin >> x >> y;
 	std::cout << "Valores introducidos:\n x = " << x << " y = " << y << '\n';
+	// Se intercambia el contenido de las dos variables
 	double temp = x;
 	x = y;
 	y = temp;
